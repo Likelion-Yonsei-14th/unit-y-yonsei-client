6b1766edc86b04c3018595a56a259e2e98ee6448
kijoo <kijoo1128@yonsei.ac.kr>
2026-05-10 22:37:08 +0900

    feat: add optional UI elements to BoothCard

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,7 +10,7 @@ function Home() {
 
             <div className="h-[17.5rem] bg-gradient-to-r from-[#1E53FF] to-[#7052FF] text-white p-6 flex flex-col justify-end">
                 <p className="text-display leading-[1.2]">2026 연세대학교 <br/> 대동제</p>
-                <p className="text-[0.875rem] font-regular mt-1">5월 27일 - 5월 29일</p>
+                <p className="text-body-2 mt-1">5월 27일 - 5월 29일</p>
 
                 <div className="mt-[1rem] flex items-center gap-[0.25rem]">
                     <img src="/home/banner index-selected.svg" alt="" />
@@ -24,7 +24,7 @@ function Home() {
                 <section className="px-4 mb-[2.25rem]">
                 <div className="flex justify-between items-center mb-5 mt-5">
                     <p className="text-heading-2 mt-[0.62rem]">현재 진행 중인 공연</p>
-                    <button className="flex items-center gap-[0.62rem] text-[#1E53FF] text-body-1 font-semibold mt-[0.62rem]">
+                    <button className="flex items-center gap-[0.62rem] text-[#1E53FF] text-body-1 mt-[0.62rem]">
                         <Link to="/performance">더보기</Link>
                         <img src="/home/addition-arrow.svg" alt="" className="w-[0.5rem] h-[0.5rem]"/>
                     </button>
@@ -44,7 +44,7 @@ function Home() {
                 <section className="px-4">
                 <div className="flex justify-between items-center mb-5">
                     <h2 className="text-heading-2">인기 부스</h2>
-                    <button className="flex items-center gap-[0.62rem] text-[#1E53FF] text-body-1 font-semibold">
+                    <button className="flex items-center gap-[0.62rem] text-[#1E53FF] text-body-1">
                         <Link to="/booth">더보기</Link>
                         <img src="/home/addition-arrow.svg" alt="" className="w-[0.5rem] h-[0.5rem]"/>
                     </button>
@@ -53,7 +53,6 @@ function Home() {
                 <div className="flex flex-col gap-[0.75rem] mb-[2.25rem]">
                     <BoothCard
                         title="호프 한잔"
-                        location="백양로"
                         waiting="대기 2팀"
                         department="사회학과"
                         boothNumber="B-07"
@@ -63,7 +62,6 @@ function Home() {
 
                     <BoothCard
                         title="호프 한잔"
-                        location="백양로"
                         waiting="대기 2팀"
                         department="사회학과"
                         boothNumber="B-07"
@@ -72,7 +70,6 @@ function Home() {
                         />
                     <BoothCard
                         title="호프 한잔"
-                        location="백양로"
                         waiting="대기 2팀"
                         department="사회학과"
                         boothNumber="B-07"
