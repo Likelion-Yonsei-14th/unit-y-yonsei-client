afbb107f72548b39678e23f95d95f1ff74a01867
문지호 <moonjiho0706@yonsei.ac.kr>
2026-05-11 20:04:18 +0900

    feat: add search bar component

---
+++ src/components/common/SearchBar.tsx
@@ -0,0 +1,39 @@
+type SearchBarProps = {
+  value: string;
+  onChange: (value: string) => void;
+  placeholder?: string;
+};
+
+export default function SearchBar({
+  value,
+  onChange,
+  placeholder = "부스명, 단체명, 메뉴명 검색",
+}: SearchBarProps) {
+  return (
+    <div className="search-bar">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="14"
+        height="14"
+        viewBox="0 0 16 16"
+        fill="none"
+      >
+        <path
+          d="M10.1112 10.1107L14.7778 14.7773M6.22228 11.6662C3.21539 11.6662 0.777832 9.22867 0.777832 6.22179C0.777832 3.2149 3.21539 0.777344 6.22228 0.777344C9.22916 0.777344 11.6667 3.2149 11.6667 6.22179C11.6667 9.22867 9.22916 11.6662 6.22228 11.6662Z"
+          stroke="#ACB1BA"
+          strokeWidth="1.55556"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+
+      <input
+        className="search-bar-input"
+        type="text"
+        value={value}
+        placeholder={placeholder}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </div>
+  );
+}
