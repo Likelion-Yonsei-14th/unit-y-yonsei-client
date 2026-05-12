5a92f90fa2d66396ac7602770f5533b6cf86ea03
Kangmin Kim <replax3221@gmail.com>
2026-05-12 17:42:32 +0900

    원상 복구

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import ReservationCard from "./pages/booth/reservation/ReservationCard";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/HomePage";
+import AvailableBoothListPage from "./pages/booth/reservation/AvailableBoothList";
 
-export default function App() {
+function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-10">
-      <ReservationCard />
+    <div className="min-h-screen bg-gray-100 flex justify-center">
+      <div className="w-full max-w-[430px] min-h-screen bg-white shadow-md">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" replace />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/reservation" element={<AvailableBoothListPage />} />
+
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
+
+export default App;
