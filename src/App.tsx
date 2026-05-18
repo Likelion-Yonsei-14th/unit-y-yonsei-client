01dffe2d4e8ea5d6481b0fc682e7f6ae489bcbc6
kijoo <kijoo1128@yonsei.ac.kr>
2026-05-18 18:08:46 +0900

    fix: delete wrong routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import BoothList from "./pages/booth/BoothList";
 import AvailableBoothListPage from "./pages/booth/reservation/AvailableBoothList";
 import LostFound from "./pages/information/LostFound";
 import BarrierFree from "./pages/information/BarrierFree";
-import BoothReservationDetailPage from "./pages/booth/reservation/BoothReservationDetailPage";
 import { InformationMainPage } from './pages/more/InformationMainPage';
 import BoothDetailPage from "./pages/booth/BoothDetailPage";
 import ReservationFormPage from "./pages/booth/reservation/ReservationFormPage";
@@ -26,7 +25,6 @@ function App() {
             <Route path="/booths/:boothNumber" element={<BoothDetailPage />} />
             <Route path="/more/lost-found" element={<LostFound />} />
             <Route path="/more/barrier-free" element={<BarrierFree />} />
-            <Route path="/booths/:boothNumber" element={<BoothReservationDetailPage />} />
             <Route path="/more" element={<InformationMainPage />} />
             <Route path="/reservation/:boothNumber" element={<ReservationFormPage />} />
             <Route path="/reservation-complete" element={<ReservationCompletePage />} />
