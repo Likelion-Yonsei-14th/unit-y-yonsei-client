a43a387da774ad0716231b7ef4379acf2c44adc9
kijoo <kijoo1128@yonsei.ac.kr>
2026-05-20 20:53:44 +0900

    feat: change floting button style

--- src/components/FloatingChatMenu.tsx
+++ src/components/FloatingChatMenu.tsx
@@ -29,7 +29,7 @@ function FloatingChatMenu() {
               rounded-[12px]
               bg-white
               w-[148px] h-[52px]
-              px-3 py-2
+              px-[12px] py-[8px]
               shadow-[0_2px_6px_rgba(0,0,0,0.07)]
             "
           >
@@ -50,7 +50,7 @@ function FloatingChatMenu() {
               bg-white
               w-[148px]
               h-[48px]
-              px-3 py-2
+              px-[12px] py-[8px]
               shadow-[0_2px_6px_rgba(0,0,0,0.07)]
             "
           >
@@ -72,7 +72,6 @@ function FloatingChatMenu() {
           flex h-[52px] w-[52px] items-center justify-center
           rounded-full
           bg-[#1E53FF]
-          shadow-lg
         "
       >
         <img
